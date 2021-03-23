b4c7d9d062878b2838249698c03db6f2d0b6822a
Otso Rasimus <orasimus@gmail.com>
2021-03-23 11:18:05 +0200

    Update to TensorFlow 2.4.1 and using valohai-cli

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,16 +1,14 @@
-import os
-
 import numpy as np
+import valohai as vh
 
+
+vh.prepare(step='Preprocess data')
 
 # Read input files from Valohai inputs directory
 # This enables Valohai to version your training data
 # and cache the data for quick experimentation
 
-inputs_path = os.getenv('VH_INPUTS_DIR', './inputs')
-input_path = os.path.join(inputs_path, 'mnist/mnist.npz')
-
-with np.load(input_path, allow_pickle=True) as file:
+with np.load(vh.inputs('mnist').path(), allow_pickle=True) as file:
     x_train, y_train = file['x_train'], file['y_train']
     x_test, y_test = file['x_test'], file['y_test']
 
@@ -21,6 +19,5 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 # This enables Valohai to version your data 
 # and upload output it to the default data store
 
-outputs_path = os.getenv('VH_OUTPUTS_DIR', './outputs')
-output_path = os.path.join(outputs_path, 'preprocessed_mnist.npz')
-np.savez(output_path, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+path = vh.outputs('dataset').path('preprocessed_mnist.npz')
+np.savez(path, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
--- train.py
+++ train.py
@@ -1,29 +1,15 @@
-import argparse
-import json
-import os
-
 import numpy as np
 import tensorflow as tf
+import valohai as vh
 
 
-# Read parameters from Valohai
-# This enables Valohai to version your parameters,
-# run quick iterations and do hyperparameter optimization
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=5)
-parser.add_argument('--learning_rate', type=float, default=0.001)
-args = parser.parse_args()
-
+vh.prepare(step='Train model')
 
 # Read input files from Valohai inputs directory
 # This enables Valohai to version your training data
 # and cache the data for quick experimentation
 
-input_path = os.getenv('VH_INPUTS_DIR', './inputs')
-f = os.path.join(input_path, 'preprocessed_mnist/preprocessed_mnist.npz')
-
-with np.load(f, allow_pickle=True) as f:
+with np.load(vh.inputs('preprocessed_mnist').path(), allow_pickle=True) as f:
     x_train, y_train = f['x_train'], f['y_train']
     x_test, y_test = f['x_test'], f['y_test']
 
@@ -34,7 +20,8 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+optimizer = tf.keras.optimizers.Adam(learning_rate=vh.parameters('learning_rate').value)
+model.compile(optimizer=optimizer,
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
@@ -44,30 +31,27 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rat
 # and for you to use it to compare experiments
 
 def log(epoch, logs):
-    print()
-    print(json.dumps({
-        'epoch': epoch,
-        'loss': str(logs['loss']),
-        'acc': str(logs['acc']),
-    }))
+    with vh.logger() as logger:
+        logger.log('epoch', epoch)
+        logger.log('accuracy', logs['accuracy'])
+        logger.log('loss', logs['loss'])
+
 
 cb = tf.keras.callbacks.LambdaCallback(on_epoch_end=log)
 
-model.fit(x_train, y_train, epochs=args.epochs, callbacks=[cb])
+model.fit(x_train, y_train, epochs=vh.parameters('epochs').value, callbacks=[cb])
 
 
 # Evaluate the model and print out the test metrics as JSON
 
 test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
-print(json.dumps({
-    'test_loss': str(test_loss),
-    'test_acc': str(test_acc),
-}))
+with vh.logger() as logger:
+    logger.log('test_accuracy', test_acc)
+    logger.log('test_loss', test_loss)
 
 
 # Write output files to Valohai outputs directory
 # This enables Valohai to version your data 
 # and upload output it to the default data store
 
-path = os.getenv('VH_OUTPUTS_DIR', './outputs')
-model.save(os.path.join(path, 'model.h5'))
+model.save(vh.outputs('model').path('model.h5'))
